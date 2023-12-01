8bbc31a34a929db1939f7e04708d98abd001e5e1
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-01 12:36:39 +0100

    Extend Board.cs class.

--- Saper-Sharp/Board.cs
+++ Saper-Sharp/Board.cs
@@ -8,10 +8,81 @@ class Board
         Random random = new Random();
 
         // Wypełnij planszę bombami.
+        for (int i = 0; i < bombCount; i++)
+        {
+            int x, y;
+            do
+            {
+                x = random.Next(0, width);
+                y = random.Next(0, height);
+            } while (board[y, x] == '*');
 
-        // Reszta kodu...
+            board[y, x] = '*';
+        }
+
+        // Reszta kodu do zainicjalizowania reszty planszy...
         return board;
     }
 
-    // Reszta kodu klasy Board...
+    public static int CountAdjacentBombs(char[,] board, int x, int y)
+    {
+        int count = 0;
+        int height = board.GetLength(0);
+        int width = board.GetLength(1);
+
+        for (int i = Math.Max(0, y - 1); i <= Math.Min(height - 1, y + 1); i++)
+        {
+            for (int j = Math.Max(0, x - 1); j <= Math.Min(width - 1, x + 1); j++)
+            {
+                if (board[i, j] == '*')
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void ExpandZeros(char[,] board, char[,] displayBoard, int x, int y)
+    {
+        int height = board.GetLength(0);
+        int width = board.GetLength(1);
+
+        for (int i = Math.Max(0, y - 1); i <= Math.Min(height - 1, y + 1); i++)
+        {
+            for (int j = Math.Max(0, x - 1); j <= Math.Min(width - 1, x + 1); j++)
+            {
+                if (displayBoard[i, j] == ' ' && board[i, j] != '*')
+                {
+                    displayBoard[i, j] = CountAdjacentBombs(board, j, i).ToString()[0];
+
+                    if (displayBoard[i, j] == '0')
+                    {
+                        ExpandZeros(board, displayBoard, j, i);
+                    }
+                }
+            }
+        }
+    }
+
+    public static bool CheckWin(char[,] displayBoard, int bombCount)
+    {
+        int uncoveredCount = 0;
+        int width = displayBoard.GetLength(1);
+        int height = displayBoard.GetLength(0);
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (displayBoard[i, j] != ' ' && displayBoard[i, j] != '*')
+                {
+                    uncoveredCount++;
+                }
+            }
+        }
+
+        return uncoveredCount == (width * height) - bombCount;
+    }
 }
