e9c79db405f1205e18a2fe1d0699677c239e6b46
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-01 12:41:24 +0100

    Start building Minesweeper class.

--- Saper-Sharp/Minesweeper.cs
+++ Saper-Sharp/Minesweeper.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Saper_Sharp
+class MinesweeperGame
 {
-    internal class Minesweeper
+    private int width;
+    private int height;
+    private int bombCount;
+    private char[,] board;
+    private char[,] displayBoard;
+    private bool gameOver;
+
+    public MinesweeperGame(int width, int height, int bombCount)
     {
+        this.width = width;
+        this.height = height;
+        this.bombCount = bombCount;
+        board = Board.InitializeBoard(width, height, bombCount);
+        displayBoard = Display.InitializeDisplayBoard(width, height);
+        gameOver = false;
     }
+
+    public void Play()
+    {
+        while (!gameOver)
+        {
+            Display.DisplayBoard(displayBoard);
+
+            Console.WriteLine("Podaj współrzędne (x, y) do odkrycia (np. 2 3): ");
+            string[] input = Console.ReadLine().Split(' ');
+
+            // Reszta kodu do obsługi gry...
+        }
+    }
+
+    // Reszta kodu klasy MinesweeperGame...
 }
