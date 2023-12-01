6aa354bf74ad2b9c34081925c1a5d2c025ea97ca
Mathias007 <mathias.najlepszy@gmail.com>
2023-12-01 12:36:24 +0100

    Add Board.cs class.

---
+++ Saper-Sharp/Board.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Board
+{
+    public static char[,] InitializeBoard(int width, int height, int bombCount)
+    {
+        char[,] board = new char[height, width];
+        Random random = new Random();
+
+        // Wypełnij planszę bombami.
+
+        // Reszta kodu...
+        return board;
+    }
+
+    // Reszta kodu klasy Board...
+}
